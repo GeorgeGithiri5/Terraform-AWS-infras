acc1395651722f1af964da84d36b9eabbb0b91f4
GeorgeGithiri5 <georgegithiri002@gmail.com>
2023-09-05 08:45:53 +0300

    aws_security_group_rule

--- project-1/main.tf
+++ project-1/main.tf
@@ -12,11 +12,21 @@ resource "aws_vpc" "main-test" {
   }
 }
 
-resource "aws_subnet" "main-subnet"{
-    vpc_id = aws_vpc.main_test.id
-    cidr_block = "10.0.1.0/24"
+resource "aws_security_group_rule" "sg_main" {
+  type              = "ingress"
+  from_port         = 0
+  to_port           = 65535
+  protocol          = "tcp"
+  cidr_blocks       = [aws_vpc.main-test.cidr_block]
+  ipv6_cidr_blocks  = [aws_vpc.main-test.ipv6_cidr_block]
+  security_group_id = "sg-123456"
+}
 
-    tags = {
-        Name = "Main"
-    }
+resource "aws_subnet" "main-subnet" {
+  vpc_id     = aws_vpc.main_test.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "Main"
+  }
 }
